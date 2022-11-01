84a491e5faa7acb256330b6e52902dd13ec86bd6
Jordan Halfman <jordanhalfman@gmail.com>
2022-11-01 16:07:32 -0700

    commit tree b0679ee380a1d2a2b526849b38e81a0abc5fdd2e

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,11 @@
+class StudentsController < ApplicationController
+
+    def index
+        render json: Student.all
+    end
+
+    def grades
+        render json: Student.all.order('grade DESC')
+    end
+
+end
